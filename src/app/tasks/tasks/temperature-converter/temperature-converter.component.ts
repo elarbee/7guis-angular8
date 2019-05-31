d18907c829d2248d53ad69f3d3fa4fc219e68b36
Alex Elarbee <alex.elarbee@accesscfa.com>
2019-05-31 18:01:34 -0400

    Add temperature conversion task

--- src/app/tasks/tasks/temperature-converter/temperature-converter.component.ts
+++ src/app/tasks/tasks/temperature-converter/temperature-converter.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-temperature-converter',
-  templateUrl: './temperature-converter.component.html',
-  styleUrls: ['./temperature-converter.component.scss']
+  templateUrl: './temperature-converter.component.html'
 })
-export class TemperatureConverterComponent implements OnInit {
+export class TemperatureConverterComponent {
+
+  celsius = 0;
+  fahrenheit = 0;
+
+  celsiusToFahrenheit = n => n * (9 / 5) + 32;
+  fahrenheitToCelsius = n => (n - 32) * (5 / 9);
 
   constructor() { }
 
-  ngOnInit() {
+  fahrenheitInput(f: number) {
+    f = +f;
+    this.celsius = +this.fahrenheitToCelsius(f).toFixed(1);
+  }
+
+  celsiusInput(c: number) {
+    c = +c;
+    this.fahrenheit = +this.celsiusToFahrenheit(c).toFixed(1);
   }
 
 }
