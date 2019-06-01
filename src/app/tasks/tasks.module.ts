f37bff6aeb201d937cbcf672211b22438087b0fc
Alex Elarbee <alex.elarbee@accesscfa.com>
2019-05-31 23:12:29 -0400

    Add prefix filter pipe to tasks module

--- src/app/tasks/tasks.module.ts
+++ src/app/tasks/tasks.module.ts
@@ -4,13 +4,13 @@ import { CounterComponent } from './tasks/counter/counter.component';
 import { TemperatureConverterComponent } from './tasks/temperature-converter/temperature-converter.component';
 import { FlightBookerComponent } from './tasks/flight-booker/flight-booker.component';
 import { TimerComponent } from './tasks/timer/timer.component';
-import { CRUDComponent } from './tasks/crud/crud.component';
+import { CRUDComponent, PrefixFilterPipe } from './tasks/crud/crud.component';
 import { CircleDrawerComponent } from './tasks/circle-drawer/circle-drawer.component';
 import { CellsComponent } from './tasks/cells/cells.component';
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [CounterComponent, TemperatureConverterComponent, FlightBookerComponent, TimerComponent, CRUDComponent, CircleDrawerComponent, CellsComponent],
+  declarations: [CounterComponent, TemperatureConverterComponent, FlightBookerComponent, TimerComponent, CRUDComponent, CircleDrawerComponent, CellsComponent, PrefixFilterPipe],
   imports: [
     CommonModule,
     FormsModule
