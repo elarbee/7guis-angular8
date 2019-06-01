d6339b7ce06ee52d4098a1f45442b0413c9547ed
Alex Elarbee <alex.elarbee@accesscfa.com>
2019-05-31 20:18:21 -0400

    Add timer task

--- src/app/tasks/tasks/timer/timer.component.ts
+++ src/app/tasks/tasks/timer/timer.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {interval} from 'rxjs';
+import {filter} from 'rxjs/operators';
 
 @Component({
   selector: 'app-timer',
@@ -7,9 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TimerComponent implements OnInit {
 
+  duration = 50;
+  maxTime = 100;
   constructor() { }
 
+  // https://www.learnrxjs.io/operators/creation/interval.html
   ngOnInit() {
+    const source = interval(10).pipe(filter(() => this.duration <= this.maxTime));
+    source.subscribe(() => this.duration += .01);
+  }
+
+  checkMaxExceeded() {
+    const greaterThanMax = this.duration > this.maxTime;
+    this.duration = greaterThanMax ? this.maxTime : this.duration;
   }
 
 }
