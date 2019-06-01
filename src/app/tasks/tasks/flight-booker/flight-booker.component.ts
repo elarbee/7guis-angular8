ac057ff9af5c2302839b3f90eee7bb842d99b8cc
Alex Elarbee <alex.elarbee@accesscfa.com>
2019-05-31 20:13:42 -0400

    Add flight booker task

--- src/app/tasks/tasks/flight-booker/flight-booker.component.ts
+++ src/app/tasks/tasks/flight-booker/flight-booker.component.ts
@@ -7,9 +7,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FlightBookerComponent implements OnInit {
 
+  roundTrip = false;
+
+  tomorrow = new Date(Date.now() + 86400000);
+
+  start = [
+    this.tomorrow.getFullYear(),
+    ('0' + (this.tomorrow.getMonth() + 1)).slice(-2),
+    ('0' + this.tomorrow.getDate()).slice(-2)
+  ].join('-');
+
+  end = this.start;
+
+  startDate: Date;
+  endDate: Date;
+
+  flightBooked: string;
+
   constructor() { }
 
   ngOnInit() {
+    this.startDate = this.stringToDate(this.start);
+    this.endDate = this.stringToDate(this.end);
+  }
+
+  calculateStartDate(start: string) {
+    this.startDate = this.stringToDate(start);
+  }
+
+  calculateEndDate(end: string) {
+    this.endDate = this.stringToDate(end);
+  }
+
+  stringToDate(s: string) {
+    const split = s.split('-');
+    return new Date(+split[0], +split[1] - 1, +split[2]);
+  }
+
+  bookFlight() {
+    const oneWayAlert = `Flight booked for ${this.startDate.toDateString()}!`;
+    const roundTripAlert = `Flight booked for ${this.startDate.toDateString()} through ${this.endDate.toDateString()}!`;
+    this.flightBooked = this.roundTrip ? roundTripAlert : oneWayAlert;;
+  }
+
+  datesValid() {
+    const startValid = this.startDate instanceof Date && !isNaN(+this.startDate);
+    const endValid = this.endDate instanceof Date && !isNaN(+this.endDate);
+    return startValid && endValid;
   }
 
 }
