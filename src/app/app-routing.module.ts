50af82c94cadd9a555225d10da17b23f23c2cc4b
Alex Elarbee <alex.elarbee@accesscfa.com>
2019-07-20 18:06:16 -0400

    Direct user to 7guis route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:  '', redirectTo:  '7guis', pathMatch:  'full' },
+  {
+    path: '7guis',
+    component: AppComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+
 })
 export class AppRoutingModule { }
